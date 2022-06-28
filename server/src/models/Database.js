bf13bb3f84c393e47a1fee34bc8a0042aaaadb46
Hadarr16 <72667509+Hadarr16@users.noreply.github.com>
2022-06-28 00:29:44 -0400

    Added some APIs

---
+++ server/src/models/Database.js
@@ -0,0 +1,12 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = ''; // Fill in MongoDB server url
+const client = new MongoClient(url);
+
+module.exports = {
+    connect: () => {
+        return client.connect();},
+    close: () => {
+        return client.close();
+    },
+    mongoDB: client.db(''), // Fill in DB name
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 const userRouter = require('./routes/userRouter');
 const notesRouter = require('./routes/notesRouter');
 
-app.use('/api/users/', userRouter);
-app.use('/api/notes/', notesRouter);
+app.use('/api/users', userRouter);
+app.use('/api/notes', notesRouter);
 
-app.listen(5000, console.log("Server running on port 5000"));
+app.use((req, res, next) =>
+{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+
+app.listen(5000, function(err) {
+    if (err) console.log(err);
+    console.log("Server listening on port 5000");
+});
