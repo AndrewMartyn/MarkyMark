def86c14457ff664b85e2bd69da7757703f93b69
GeorgeCacoulidis <ge0rgec@protonmail.com>
2022-06-30 12:53:12 -0400

    tiny changes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # markdown-notes
+
 - A web-based markdown editor written using a MERN stack
 
 ## Usage
+
 heroku instructions go here...
--- server/src/routes/notesRouter.js
+++ server/src/routes/notesRouter.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const database = require('./models/Database.js');
+const express = require("express");
+const database = require("./models/Database");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
@@ -10,111 +10,113 @@ notesRouter.use(bodyParser.json());
 database.connect();
 
 // user searches through their notes based on title and tags
-notesRouter.get('/users/:userId/notes', async (req, res) => {
-    const userId = req.params.userId;
-    const {tags, searchText} = req.body;
+notesRouter.get("/users/:userId/notes", async (req, res) => {
+  const userId = req.params.userId;
+  const { tags, searchText } = req.body;
 
-    let error;
-    let searchResults = [];
-    let search = searchText.trim();
+  let error;
+  let searchResults = [];
+  let search = searchText.trim();
 
-    try {
-        const db = database.mongoDB;
-        const results = await db.collection('Notes').find({
-            'UserID': userId,
-            'NoteTags': {$all: tags},
-            'NoteTitle': {$regex: search + '.', $options: 'i'} // trying to search case-insensitive
-        }).toArray();
+  try {
+    const db = database.mongoDB;
+    const results = await db
+      .collection("Notes")
+      .find({
+        UserID: userId,
+        NoteTags: { $all: tags },
+        NoteTitle: { $regex: search + ".", $options: "i" }, // trying to search case-insensitive
+      })
+      .toArray();
 
-        if (results.length > 0) {
-            for (let i = 0; i < results.length; i++)
-                searchResults.push({
-                    noteId: results[i].NoteID,
-                    noteTitle: results[i].NoteTitle,
-                    noteBody: results[i].NoteBody,
-                    noteTags: results[i].NoteTags
-                });
-            error = 'Note(s) found';
-        }
-        else
-            error = 'No matches found';
-    }
-    catch(e) {
-        error = e.toString();
-    }
+    if (results.length > 0) {
+      for (let i = 0; i < results.length; i++)
+        searchResults.push({
+          noteId: results[i].NoteID,
+          noteTitle: results[i].NoteTitle,
+          noteBody: results[i].NoteBody,
+          noteTags: results[i].NoteTags,
+        });
+      error = "Note(s) found";
+    } else error = "No matches found";
+  } catch (e) {
+    error = e.toString();
+  }
 
-    let ret = {results: searchResults, error: error};
-    res.status(200).json(ret);
+  let ret = { results: searchResults, error: error };
+  res.status(200).json(ret);
 });
 
 // user creates a new note
-notesRouter.post('/users/:userId/notes/:noteId', async (req, res) => {
-    const userId = req.params.userId;
-    const noteId = req.params.noteId;
-    const {noteTitle, noteBody, noteTags} = req.body;
+notesRouter.post("/users/:userId/notes/:noteId", async (req, res) => {
+  const userId = req.params.userId;
+  const noteId = req.params.noteId;
+  const { noteTitle, noteBody, noteTags } = req.body;
 
-    let error;
+  let error;
 
-    let newNote = {UserID: userId, NoteID: noteId, NoteTitle: noteTitle, NoteBody: noteBody, NoteTags: noteTags}
+  let newNote = {
+    UserID: userId,
+    NoteID: noteId,
+    NoteTitle: noteTitle,
+    NoteBody: noteBody,
+    NoteTags: noteTags,
+  };
 
-    try {
-        const db = database.mongoDB;
-        await db.collection('Notes').insertOne(newNote);
-        error = 'Note created';
-    }
-    catch (e) {
-        error = "Server error:\n" + e.toString();
-    }
+  try {
+    const db = database.mongoDB;
+    await db.collection("Notes").insertOne(newNote);
+    error = "Note created";
+  } catch (e) {
+    error = "Server error:\n" + e.toString();
+  }
 
-    res.status(200).json({error: error});
+  res.status(200).json({ error: error });
 });
 
 // user deletes a note
-notesRouter.delete('/users/:userId/notes/:noteId', async (req, res) => {
-    const userId = req.params.userId;
-    const noteId = req.params.noteId;
+notesRouter.delete("/users/:userId/notes/:noteId", async (req, res) => {
+  const userId = req.params.userId;
+  const noteId = req.params.noteId;
 
-    let error;
-    const deleteMe = {UserID: userId, NoteId: noteId};
+  let error;
+  const deleteMe = { UserID: userId, NoteId: noteId };
 
-    try {
-        const db = database.mongoDB;
-        await db.collection('Notes').findOneAndDelete(deleteMe);
-        error = 'Note deleted';
-    }
-    catch (e) {
-        error = "Server error:\n" + e.toString();
-    }
+  try {
+    const db = database.mongoDB;
+    await db.collection("Notes").findOneAndDelete(deleteMe);
+    error = "Note deleted";
+  } catch (e) {
+    error = "Server error:\n" + e.toString();
+  }
 
-    res.status(200).json({error: error});
+  res.status(200).json({ error: error });
 });
 
 // user saves updates to a note
-notesRouter.put('/users/:userId/notes/:noteId', async (req, res) => {
-    const userId = req.params.userId;
-    const noteId = req.params.noteId;
-    const {title, body, tags} = req.body;
+notesRouter.put("/users/:userId/notes/:noteId", async (req, res) => {
+  const userId = req.params.userId;
+  const noteId = req.params.noteId;
+  const { title, body, tags } = req.body;
 
-    let error;
+  let error;
 
-    let edits = {};
-    if (title != null)
-        edits.NoteTitle = title;
-    if (body != null)
-        edits.NoteBody = body;
-    if (tags != null)
-        edits.NoteTags = tags;
+  let edits = {};
+  if (title != null) edits.NoteTitle = title;
+  if (body != null) edits.NoteBody = body;
+  if (tags != null) edits.NoteTags = tags;
 
-    try {
-        const db = database.mongoDB;
-        await db.collection('Notes').updateOne({UserID: userId, NoteID: noteId}, {$set: edits});
-        error = 'Note updated';
-    }
-    catch (e) {
-        error = "Server error:\n" + e.toString();
-    }
+  try {
+    const db = database.mongoDB;
+    await db
+      .collection("Notes")
+      .updateOne({ UserID: userId, NoteID: noteId }, { $set: edits });
+    error = "Note updated";
+  } catch (e) {
+    error = "Server error:\n" + e.toString();
+  }
 
-    res.status(200).json({error: error});
+  res.status(200).json({ error: error });
 });
 
 database.close();
