d154a5575269a5823ee438702a3b50bb9ff1b2a2
Hadarr16 <72667509+Hadarr16@users.noreply.github.com>
2022-06-30 22:48:10 -0400

    Added PUT as allowed method in header

--- server/src/server.js
+++ server/src/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) =>
     );
     res.setHeader(
         'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS'
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS'
     );
     next();
 });
