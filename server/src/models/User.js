a491a0a27dbb8b0b4215d58c51401ae9fb92d515
Darren Ha <72667509+Hadarr16@users.noreply.github.com>
2022-07-07 11:56:53 -0400

    Added 'tags' to user schema

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -18,6 +18,10 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    // array of strings to store all the tags used by a particular user for easier filtered search
+    tags: {
+        type: [String]
+    },
     dateCreated: {
         type: Date,
         default: Date.now()
