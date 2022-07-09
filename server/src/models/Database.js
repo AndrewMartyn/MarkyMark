c1c66bd11a389654ebc8a06329a0e60665bd38b7
Hadarr16 <72667509+Hadarr16@users.noreply.github.com>
2022-07-09 12:56:53 -0400

    moved .env

--- server/src/models/Database.js
+++ server/src/models/Database.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const url = process.env.MONGODB_URI;
-// const url = 'mongodb+srv://HunterMCB:94OO81APQii0KtTy@markeymark.p2cjyug.mongodb.net/?retryWrites=true&w=majority';
-// console.log("url: " + url);
+ console.log("url: " + url);
 const client = new MongoClient(url); // url undefined for some reason
 
 module.exports = {
