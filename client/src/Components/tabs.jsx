29167423f7f396d89842fdb974acf63cb197e24d
Ferom72 <59715577+Ferom72@users.noreply.github.com>
2022-07-13 03:00:08 -0400

    Add files via upload

--- client/src/Components/tabs.jsx
+++ client/src/Components/tabs.jsx
@@ -1,50 +1,27 @@
-import React from 'react'
-import { Tabs,Tab,Typography,Box } from '@mui/material'
-
-function TabPanel(props = TabPanelProps) {
-    const { children, value, index, ...other } = props;
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../StyleSheets/tabs.css'
+import Dropdown from 'react-bootstrap/Dropdown';
   
-    return (
-      <div
-        className='menu'
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ padding: 0,margin:0}}>
-            <Typography sx={{ padding: 0,margin:0}}>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-  function a11yProps(index = number) {
-    return {
-      id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    };
-  }
-  
-  export default function BasicTabs() {
-    const [value, setValue] = React.useState(0);
-  
-    const handleChange = (event = React.SyntheticEvent, newValue =number) => {
-      setValue(newValue);
-    };
-  
-    return (
-      <Box sx={{ margin: 0, padding: 0, width: '100%' }}>
-        <Box sx={{ margin:0,padding:0,borderBottom: 0.1, borderColor: 'divider' }}>
-          <Tabs sx={{margin:0,padding:0}} value={value} onChange={handleChange} aria-label="basic tabs example">
-            <Tab sx={{fontSize:10, margin:0,padding:0}} label="File" {...a11yProps(0)} />
-            <Tab sx={{fontSize:10, margin:0,padding:0}} label="Edit" {...a11yProps(1)} />
-            <Tab sx={{fontSize:10, margin:0,padding:0}} label="Help" {...a11yProps(2)} />
-          </Tabs>
-        </Box>
-      </Box>
-    );
-  }
+export default function Tabs(props) {
+  return (
+    <div className='dropdownContainer'>
+      <Dropdown>
+        <Dropdown.Toggle id="dropdown-button-dark-example1" variant="outline-secondary" size='sm' style={{backgroundColor: 'transparent',border: 'none', padding:'0.3em',marginLeft:'0.5em', marginRight:'0.5em'}}>
+          {props.name}
+        </Dropdown.Toggle>
+        <Dropdown.Menu className='super-colors' variant='dark'>
+          <Dropdown.Item href="#">
+            New File
+          </Dropdown.Item>
+          <Dropdown.Item href="#">
+            Open File
+          </Dropdown.Item>
+          <Dropdown.Item href="#">
+            Open Folder
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
+}
