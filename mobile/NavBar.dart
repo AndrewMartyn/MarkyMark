b0f2ea930261d8b06d7586e8062af87277f21439
whiteandrea <81045968+whiteandrea@users.noreply.github.com>
2022-07-14 18:27:52 -0400

    Update NavBar.dart

--- mobile/NavBar.dart
+++ mobile/NavBar.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: [],
+      child: Container(
+        color: Color.fromARGB(255, 17, 29, 45),
+        child: ListView(
+          children: [],
+        ),
       ),
     );
   }
 }
+ 
