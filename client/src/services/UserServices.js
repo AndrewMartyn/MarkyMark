ee25392a6ae28a53a9561cdd2c71de30494a7fef
Ferom72 <59715577+Ferom72@users.noreply.github.com>
2022-07-22 13:56:29 -0400

    Add files via upload

--- client/src/services/UserServices.js
+++ client/src/services/UserServices.js
@@ -1,38 +1,6 @@
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+const axios = require("axios");
 
-export async function doLogin2(event) {}
-
-export async function doLogin(event) {
-    event.preventDefault();
-    console.log(event.password);
-
-    let obj = { email: email.value, password: password.value };
-
-    try {
-        const response = await fetch(
-            `http://localhost:5001/api/users/?email=${obj.email}&password=${obj.password}`
-        );
-        let res = JSON.parse(await response.text());
-
-        if (res.error === "No Such Records") {
-            setWarning(true);
-        } else {
-            setWarning(false);
-            let user = {
-                firstName: res.firstName,
-                lastName: res.lastName,
-                tags: res.tags,
-                id: res.id,
-            };
-            localStorage.setItem("user_data", JSON.stringify(user));
-            navigation("texteditor");
-        }
-    } catch (e) {
-        console.log(e.toString());
-        return;
-    }
-}
+export async function loginUser() {}
 
 export async function registerUser() {}
 
