ab7a8b2ac185c378a924d12f83f1739e44957096
Ferom72 <59715577+Ferom72@users.noreply.github.com>
2022-07-25 17:34:33 -0400

    Add files via upload

--- client/src/Components/Tree/File/TreeFile.js
+++ client/src/Components/Tree/File/TreeFile.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import { AiOutlineFile, AiOutlineDelete, AiOutlineEdit } from "react-icons/ai/index.js";
+import { AiOutlineFile, AiOutlineDelete, AiOutlineEdit,AiFillSave } from "react-icons/ai/index.js";
 
 import { StyledFile } from "../File/TreeFile.style.js";
 import { useTreeContext } from "../state/TreeContext.js";
@@ -9,7 +9,8 @@ import { PlaceholderInput } from "../TreePlaceholderInput.js";
 import { FILE } from "../state/constants.js";
 import FILE_ICONS from "../FileIcons.js";
 
-const File = ({ name, id, node}) => {
+const File = ({ name, id, node,setDoc,doc}) => {
+
   const { dispatch, isImparative, onNodeClick } = useTreeContext();
   const [isEditing, setEditing] = useState(false);
   const ext = useRef("");
@@ -22,6 +23,7 @@ const File = ({ name, id, node}) => {
     dispatch({ type: FILE.EDIT, payload: { id, name } });
     setEditing(false);
   };
+
   const commitDelete = async () => {
       try{
         let userInfo = JSON.parse(window.localStorage.getItem('user_data'))
@@ -37,23 +39,69 @@ const File = ({ name, id, node}) => {
 
         const response = await fetch(`http://localhost:5001/api/users/${userInfo.userId}/notes`, {method:'DELETE',body:js,headers:{'Content-Type': 'application/json'}});
 
-        let res = JSON.parse(await response.text());
-    
-        console.log(res)
 
       }catch(e){
         alert(e.toString())
         return
       }
   };
-  const handleNodeClick = React.useCallback(
-    (e) => {
-      e.stopPropagation();
-      onNodeClick({ node });
-    },
-    [node]
 
-  );
+  const handleNodeClick = React.useCallback(async()=>{
+
+      onNodeClick({ node });
+
+      try{
+
+        let userInfo = JSON.parse(window.localStorage.getItem('user_data'))
+        
+        const response = await fetch(`http://localhost:5001/api/users/${userInfo.userId}/notes?searchText=${node.name}&tags[]=&jwtToken=`,{method:'GET',headers:{'Content-Type': 'application/json'}});
+  
+        let res = JSON.parse(await response.text())
+
+        let found = res.results.find(file => file.noteId === node.id)
+
+        console.log("we set the doc")
+        console.log(found)
+
+        setDoc(found.noteBody)
+
+      }catch(e){
+        alert(e.toString())
+        return
+      }
+  });
+
+  const handleNodeSave = React.useCallback(async()=>{
+
+    let body = JSON.parse(window.localStorage.getItem('user_body'))
+
+    console.log(body)
+    
+    try{
+    
+      let userInfo = JSON.parse(window.localStorage.getItem('user_data'))
+      
+      await fetch(`http://localhost:5001/api/users/${userInfo.userId}/notes?searchText=${node.name}&tags[]=&jwtToken=`,{method:'GET',headers:{'Content-Type': 'application/json'}});
+
+      let object = {
+          name:node.name,
+          body:body,
+          tags: [],
+      }
+
+      let js = JSON.stringify(object)
+      
+      await fetch(`http://localhost:5001/api/users/${userInfo.userId}/notes?noteId=${node.id}`,{method:'PUT',body :js,headers:{'Content-Type': 'application/json'}});
+      
+      console.log('we saved the body')
+
+    }
+    catch(e){
+      alert(e.toString())
+      return
+    }
+});
+
   const handleCancel = () => {
     setEditing(false);
   };
@@ -82,6 +130,7 @@ const File = ({ name, id, node}) => {
             <div className="actions">
               <AiOutlineEdit onClick={toggleEditing} />
               <AiOutlineDelete onClick={commitDelete} />
+              <AiFillSave onClick={handleNodeSave}/>
             </div>
           )}
         </ActionsWrapper>
