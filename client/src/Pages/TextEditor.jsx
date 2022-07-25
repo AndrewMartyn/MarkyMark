515b6af36a3efccdd8d5ddce5e00b2bf8cd03298
Ferom72 <59715577+Ferom72@users.noreply.github.com>
2022-07-24 21:45:51 -0400

    Add files via upload

--- client/src/Pages/TextEditor.jsx
+++ client/src/Pages/TextEditor.jsx
@@ -6,71 +6,80 @@ import "../StyleSheets/splitpane.css";
 import Tabs from '../Components/tabs'
 import FileTree from "../Components/FileTree.jsx";
 import '../StyleSheets/App.css'
+import Container from "react-bootstrap/esm/Container";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 
 function TextEditor() {
 
-    const [doc, setDoc] = useState(`# Text`);
+    const [doc, setDoc] = useState(``);
     const [createfile,setCreateFile] = useState({fileName:'',type:''})
     const [clickedFile,setClickedFile] = useState('nothing')
 
     const fileClicked = clickedFile === 'nothing' ? true : false
-    var mins = 1, the_interval = mins * 60 * 1000
+    // var mins = 1, the_interval = mins * 60 * 1000
 
     const handleDocChange = useCallback((newDoc) => {
         setDoc(newDoc);
     }, []);
 
+   
 
-    if(fileClicked !== true){
+    // if(fileClicked !== true){
 
-        
-
-        
-        setTimeout(async function(){
-            try{
+    //     setTimeout(async function(){
+    //         try{
     
-                let userInfo = JSON.parse(window.localStorage.getItem('user_data'))
+    //             let userInfo = JSON.parse(window.localStorage.getItem('user_data'))
                 
-                await fetch(`http://localhost:5001/api/users/${userInfo.userId}/notes?searchText=${clickedFile.node.name}&tags[]=&jwtToken=`,{method:'GET',headers:{'Content-Type': 'application/json'}});
+    //             await fetch(`http://localhost:5001/api/users/${userInfo.userId}/notes?searchText=${clickedFile.node.name}&tags[]=&jwtToken=`,{method:'GET',headers:{'Content-Type': 'application/json'}});
           
-                let object = {
-                    name:clickedFile.node.name,
-                    body:doc,
-                    tags: [],
-                }
+    //             let object = {
+    //                 name:clickedFile.node.name,
+    //                 body:doc,
+    //                 tags: [],
+    //             }
         
-                let js = JSON.stringify(object)
+    //             let js = JSON.stringify(object)
                 
-                await fetch(`http://localhost:5001/api/users/${userInfo.userId}/notes?noteId=${clickedFile.node.id}`,{method:'PUT',body : js,headers:{'Content-Type': 'application/json'}});
+    //             await fetch(`http://localhost:5001/api/users/${userInfo.userId}/notes?noteId=${clickedFile.node.id}`,{method:'PUT',body :js,headers:{'Content-Type': 'application/json'}});
                 
-                
+    //             console.log('we updated the body')
 
-              }
-              catch(e){
-                alert(e.toString())
-                return
-              }
-        },the_interval)
-    }else{
+    //           }
+    //           catch(e){
+    //             alert(e.toString())
+    //             return
+    //           }
+    //     },the_interval)
+    // }else{
 
-    }
+    // }
 
     return (
         <div className="App" >
             <div style={{backgroundColor:'rgb(30,34,39)'}}>
-                <Tabs file = {setCreateFile} name='File'/>
-                <Tabs name='Edit'/>
-                <Tabs name='Help'/>
-                {fileClicked ? <></> : <span style={{color:'rgb(157,165,180)',position:'relative',left:'45%',transform:'translate(-45%)'}}>{clickedFile.node.name}</span>}
-                
+                <Container fluid>
+                    <Row class = 'row'>
+                        <Col md={3} style={{textAlign:'start', padding:'0'}}>
+                            <Tabs file = {setCreateFile} name='File' newFile='New File' openFile = 'Open File' save = 'Save' saveAs= 'Save as'/>
+                            <Tabs name='Edit' undo= 'Undo' redo = 'Redo' cut = 'Cut' copy='Copy' paste ='Paste'/>
+                        </Col>
+                        <Col md ={7} style={{ textAlign:'center' }}>{fileClicked ? <></> : <Tabs name ={clickedFile.node.name} file={true} style={{color:'rgb(157,165,180)'}}/>}</Col>
+                        <Col md ={1} style={{ textAlign:'end'}}>
+                            <Tabs name='User'/>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
     
             <SplitPane split="vertical" minSize="0" defaultSize="10%" style={{position:'static'}}>
                 <div style={{backgroundColor:'rgb(30,34,39)', height: '100vh'}}>
-                    <FileTree clicked ={setClickedFile}/>
+                    <FileTree clicked ={setClickedFile} doc={doc} setDoc={setDoc}/>
                 </div>
                 <SplitPane split="vertical" minSize="50%" defaultSize="50%" style={{position:'static'}}>
-                    <Editor onChange={handleDocChange} doc={doc} />
+                    <Editor onChange={handleDocChange} doc={doc} clicked ={fileClicked} />
                     <Preview doc={doc}/>
                 </SplitPane>
             </SplitPane>
