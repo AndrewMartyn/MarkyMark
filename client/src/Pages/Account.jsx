e5a87656c08ad8b1e3f2ca31ddf36bf73c73a798
Ferom72 <romerof0005@gmail.com>
2022-07-28 02:41:38 -0400

    Updates

--- client/src/Pages/Account.jsx
+++ client/src/Pages/Account.jsx
@@ -48,6 +48,7 @@ export default function Account() {
                     setSuccess(false);
                 }
             } catch (e) {
+                
                 console.log(e.toString());
             }
         } else {
